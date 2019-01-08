d3ea5089a53fef8bb879708d016724e08c6a92ed
HuyCL <43223817+HuyCL@users.noreply.github.com>
2019-01-08 13:08:17 -0500

    Add files via upload

---
+++ ws01/athome/w1_home.c
@@ -0,0 +1,10 @@
+/*Name: Huy Lam		MyID: 115705162		Email: hlam25@myseneca.ca*/
+#define _CRT_SECURE_NO_WARNINGS_
+#include <stdio.h>
+
+int main(void) {
+	printf("********************************\n");
+	printf("*** Welcome to C Programming ***\n");
+	printf("********************************\n");
+	return 0;
+}
